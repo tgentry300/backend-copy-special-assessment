4bc90ff4d95d083e27f4317b21a7701070756ed0
tgentry300 <taylor.gentry300@gmail.com>
2018-11-15 10:09:10 -0500

    implemented 3 functions first prints file paths, second copies to args specified directory, third takes files in directory and zips them into new file

--- copyspecial.py
+++ copyspecial.py
@@ -12,12 +12,52 @@ import os
 import shutil
 import commands
 import argparse
+import subprocess
 
 """Copy Special exercise
 """
 
-# +++your code here+++
 # Write functions and modify main() to call them
+
+def get_special_paths(dir):
+    """returns list of special files meeting parameters"""
+    abs_path_list = []
+    for _root, _dirs, files in os.walk(dir):
+        for file in files:
+            abs_path_list.append(os.path.abspath(file))
+        
+    regex = re.compile(r'\w+__\w+_.\..+')
+    special_files = filter(regex.search, abs_path_list)
+    return special_files
+
+def to_dir(new_dir):
+    special_files = get_special_paths('./')
+    
+    for file in special_files:
+        if not os.path.exists(new_dir):
+            os.makedirs(new_dir)
+            shutil.copy(file, new_dir)
+        else: 
+            shutil.copy(file, new_dir)
+
+
+
+def to_zip(file_name):
+    """Takes a file_name from command line and zips all files in
+        directory into file_name"""
+
+    command = 'zip -j {} '.format(file_name)
+    special_files = get_special_paths('./')
+
+    basenames = [os.path.basename(file) for file in special_files]
+
+    print command + ' '.join(basenames)
+
+    os.system(command + ' '.join(basenames))
+
+    print commands.getoutput(command + ' '.join(basenames))
+
+
 
 def main():
     # This snippet will help you get started with the argparse module.
@@ -34,8 +74,17 @@ def main():
     # This is input data validation.  If something is wrong (or missing) with any
     # required args, the general rule is to print a usage message and exit(1).
 
-    # +++your code here+++
-    # Call your functions
+    new_dir = args.todir
+    zip_file = args.tozip
+
+    if new_dir:
+        to_dir(new_dir)
+
+    elif zip_file:
+        to_zip(zip_file)
+
+    else:
+        print '\n'.join(get_special_paths('./')) + '\n'
   
 if __name__ == "__main__":
     main()
